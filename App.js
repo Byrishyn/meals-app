2c7264b75e8c26042b4559a837a55adc659f5aff
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-07-04 15:17:30 +0900

    Add Navigator to App

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from "expo-font"
 import AppLoading from 'expo-app-loading';
+import MealsNavigator from "./navigation/MealsNavigator"
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -17,16 +18,15 @@ export default function App() {
   if (!fontLoaded) {
     return (
       <AppLoading
-      startAsync={fetchFonts}
-      onFinish={()=> setFontLoaded(true)}
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={err => console.log(err)
+        }
       />
     )
   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MealsNavigator />
   );
 }
 
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,1 +1,14 @@
 import { createStackNavigator } from "react-navigation-stack"
+import { createAppContainer} from "react-navigation"
+
+import CategoriesScreen from "../screens/CategoriesScreen"
+import CategoryMealsScreen from "../screens/CategoryMealsScreen"
+import MealsDetailScreen from "../screens/MealsDetailScreen"
+
+const MealsNavigator = createStackNavigator({
+    Categories: {screen: CategoriesScreen},
+    CategoryMeals: {screen: CategoryMealsScreen},
+    MealsDetail: {screen: MealsDetailScreen},
+});
+
+export default createAppContainer(MealsNavigator)
