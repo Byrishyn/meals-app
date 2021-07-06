5e4872df3517bc2b8efe45e5315b145a61d99b38
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-07-07 08:28:26 +0900

    Add tab-navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,21 +1,31 @@
 import { createStackNavigator } from "react-navigation-stack"
-import { createAppContainer} from "react-navigation"
-import {Platform} from "react-native"
+import { createBottomTabNavigator } from "react-navigation-tabs"
+import { createAppContainer } from "react-navigation"
+import { Platform } from "react-native"
 import Colors from "../constants/Colors"
- 
+
 import CategoriesScreen from "../screens/CategoriesScreen"
 import CategoryMealsScreen from "../screens/CategoryMealsScreen"
 import MealsDetailScreen from "../screens/MealsDetailScreen"
+import FavoritesScreen from "../screens/FavoritesScreen"
 
 const MealsNavigator = createStackNavigator({
-    Categories: {screen: CategoriesScreen, navigationOptions: {headerTitle: "Meal Categories"}},
-    CategoryMeals: {screen: CategoryMealsScreen},
-    MealDetail: {screen: MealsDetailScreen},
-    
-}, {defaultNavigationOptions : {
-    headerStyle : {
+  Categories: { screen: CategoriesScreen, navigationOptions: { headerTitle: "Meal Categories" } },
+  CategoryMeals: { screen: CategoryMealsScreen },
+  MealDetail: { screen: MealsDetailScreen },
+
+}, {
+  defaultNavigationOptions: {
+    headerStyle: {
       backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
     },
-    headerTintColor : Platform.OS === "android" ? "white" : Colors.primaryColor }});
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+  }
+});
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals : MealsNavigator,
+  Favorites : FavoritesScreen
+})
+
+export default createAppContainer(MealsFavTabNavigator)
