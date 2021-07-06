e5878d27e89596408274a0daed82287a2ef6dc76
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-07-06 23:01:50 +0900

    Add header button

--- components/MealComponent.js
+++ components/MealComponent.js
@@ -35,7 +35,8 @@ const styles = StyleSheet.create({
         width: "100%",
         backgroundColor: "#e5e5e5",
         borderRadius: 10,
-        overflow: "hidden"
+        overflow: "hidden",
+        marginVertical: 10,
     },
     mealHeader: {
         height: "85%"
--- screens/MealsDetailScreen.js
+++ screens/MealsDetailScreen.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { View, Text, StyleSheet, Button } from "react-native"
 import { MEALS } from "../data/dummy-data"
+import { HeaderButtons, Item } from "react-navigation-header-buttons"
+import HeaderButton from "../components/HeaderButton"
 
 const MealsDetailScreen = props => {
     const mealId = props.navigation.getParam("mealId")
@@ -22,7 +24,9 @@ MealsDetailScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam("mealId")
     const selectedMeal = MEALS.find((meal) => meal.id === mealId)
     return {
-        headerTitle: selectedMeal.title
+        headerTitle: selectedMeal.title,
+        headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}><Item title="Favorite" iconName="ios-star" onPress={() => {console.log("favorite");
+        }}/></HeaderButtons>
     }
 }
 
