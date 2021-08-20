47854bba8920da747d88cc0c97771980dfbf4a8e
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-08-20 17:52:00 +0900

    Add redux to project

--- App.js
+++ App.js
@@ -3,7 +3,16 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from "expo-font"
 import AppLoading from 'expo-app-loading';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import mealsReducer from './store/reducers/meals';
 import MealsNavigator from "./navigation/MealsNavigator"
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer)
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -26,7 +35,9 @@ export default function App() {
     )
   }
   return (
-    <MealsNavigator />
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
 
