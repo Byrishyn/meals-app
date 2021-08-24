df271407624c4eb863ea2f21779673cef55ff315
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-08-24 12:27:59 +0900

    Add text to FavoritesScreen when no favorite meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,9 +4,19 @@ import MealList from "../components/MealList"
 import { useSelector } from "react-redux"
 
 import HeaderButton from "../components/HeaderButton"
+import { StyleSheet, View } from "react-native"
+import DefaultText from "../components/DefaultText"
 
 const FavoritesScreen = props => {
     const favMeals = useSelector(state => state.meals.favoriteMeals)
+
+    if (favMeals.length === 0) {
+        return (
+            <View style={styles.content}>
+                <DefaultText>No favorite meals found. Start adding some !</DefaultText>
+            </View>
+        )
+    }
     return (
         <MealList listData={favMeals} navigation={props.navigation} />
     )
@@ -22,6 +32,14 @@ FavoritesScreen.navigationOptions = (navData) => {
         </HeaderButtons>
     }
 }
+
+const styles = StyleSheet.create({
+    content : {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
 
 
 export default FavoritesScreen;
